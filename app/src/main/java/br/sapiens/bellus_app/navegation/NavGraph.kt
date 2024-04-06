21dc86b6d280cfd4e3e6d8cdb6bbe036b73cdcac
jackvadderr <jacjacckck@gmail.com>
2024-04-06 18:40:22 -0400

    feat: implementado popUpTo para rotas de navegação

--- app/src/main/java/br/sapiens/bellus_app/navegation/NavGraph.kt
+++ app/src/main/java/br/sapiens/bellus_app/navegation/NavGraph.kt
@@ -5,7 +5,6 @@ import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
@@ -13,7 +12,6 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import br.sapiens.bellus_app.telas.barra_navegation.BottomNavigation
-import br.sapiens.bellus_app.telas.home.NavControllerProvider
 import br.sapiens.bellus_app.telas.home.TelaMarketplace
 import br.sapiens.bellus_app.telas.login.TelaLogin
 import br.sapiens.bellus_app.telas.splash.TelaSplash
@@ -25,8 +23,8 @@ fun NavGraph(startDestination: String = RotasDestinos.Splash.rota) {
     val navController = rememberNavController()
     val navBackStackEntry by navController.currentBackStackEntryAsState()
     val currentRoute = navBackStackEntry?.destination?.route
-    val TelaNavegationviewModel: TelaNavegationBarViewModel = viewModel()
-    val selectedItem = TelaNavegationviewModel.selectedItem.observeAsState()
+    val telaNavegationviewModel: TelaNavegationBarViewModel = viewModel()
+    val selectedItem = telaNavegationviewModel.selectedItem.observeAsState()
 
     Scaffold(
         bottomBar = {
@@ -46,12 +44,12 @@ fun NavGraph(startDestination: String = RotasDestinos.Splash.rota) {
                     navigateToHome = {
                         navController.navigate(
                             route = RotasDestinos.Home.rota,
-                        )
+                        ) { popUpTo(RotasDestinos.Splash.rota) { inclusive = true} }
                     },
                     navigateToLogin = {
                         navController.navigate(
                             route = RotasDestinos.Login.rota,
-                        )
+                        ) { popUpTo(RotasDestinos.Splash.rota) { inclusive = true} }
                     }
                 )
             }
@@ -62,37 +60,31 @@ fun NavGraph(startDestination: String = RotasDestinos.Splash.rota) {
                     navigateToRegister = {
                         navController.navigate(
                             route = RotasDestinos.Register.rota,
-                        )
+                        ) { popUpTo(RotasDestinos.Login.rota) { inclusive = true} }
                     },
                     navigateToHome = {
                         navController.navigate(
                             route = RotasDestinos.Home.rota,
-                        )
+                        ) { popUpTo(RotasDestinos.Login.rota) { inclusive = true} }
                     }
                 )
             }
             // Tela Home
-//            composable(RotasDestinos.Home.rota) {
-//                TelaMarketplace(
-//                    hiltViewModel(),
-//                    navigateToSearch = {
-//                        navController.navigate(
-//                            route = RotasDestinos.Home.rota,
-//                        )
-//                    },
-//                    navigateToProfile = {
-//                        navController.navigate(
-//                            route = RotasDestinos.Perfil.rota
-//                        )
-//                    },
-//                )
-//            }
+            composable(RotasDestinos.Home.rota) {
+                TelaMarketplace(
+                    hiltViewModel(),
+                    navigateToSearch = {
+                        navController.navigate(
+                            route = RotasDestinos.Home.rota,
+                        ) { popUpTo(RotasDestinos.Home.rota) { inclusive = true} }
+                    },
+                    navigateToProfile = {
+                        navController.navigate(
+                            route = RotasDestinos.Perfil.rota
+                        ) { popUpTo(RotasDestinos.Home.rota) { inclusive = true} }
+                    },
+                )
+            }
         }
     }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun PreviewNavGraph() {
-    val navControllerProvider = NavControllerProvider().apply { this.navController = rememberNavController() }
 }
